6b318fd180bdb3ceb050574740deef88002c181e
Renan Colombo <renancolombomail@gmail.com>
2020-06-28 02:02:25 -0300

    Utf-8 & bug fix

--- src/main/java/com/focamacho/silkchest/SilkChest.java
+++ src/main/java/com/focamacho/silkchest/SilkChest.java
@@ -5,6 +5,7 @@ import com.focamacho.silkchest.events.BlockBreakEvent;
 import com.focamacho.silkchest.events.SpawnerPlaceEvents;
 import com.focamacho.silkchest.events.TooltipEvent;
 import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -40,7 +42,13 @@ public class SilkChest {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-
+        //bug fix
+        if(silkBlocks.isEmpty()) {
+            ConfigSilkChest.silkList.get().forEach(string -> {
+                Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(string));
+                if(block != null) SilkChest.silkBlocks.add(block);
+            });
+        }
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
